d402db90bbecc88ab43c7d49a17dd76812f11c87
Simon Lee <oprogfrogo@gmail.com>
2018-08-06 21:35:14 -0700

    Adding a sign in link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'profile/index'
   root to: 'welcome#index'
+
+  resources :welcome, :profiles
+  
   devise_for :users
-  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
